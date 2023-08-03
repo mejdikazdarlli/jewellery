4ca2261ce99bb8aa24cd74b715b2861d00633b80
mejdikazdarlli <89795915+mejdikazdarlli@users.noreply.github.com>
2023-08-03 23:36:42 +0100

    add perry.html

--- js/index.js
+++ js/index.js
@@ -1,18 +1,12 @@
 function _(elm){return document.getElementById(elm)}
-// window.addEventListener("scroll",function(){
-//     var nav = document.querySelector("custom-header-jewel");
-//     if(window.scrollY > 0){console.log(nav)}
-//   //nav.classList.toggle("sticky",window.scrollY > 0)
-// })
-
-var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
-    console.log("hereeeeeeeeeeeeeee")
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.querySelector("custom-header-jewel").style.top = "0";
-  } else {
-    document.querySelector("custom-header-jewel").style.top = "-50px";
+window.addEventListener("scroll",function(){
+  let nav = document.querySelector(".custom-header-jewel");
+  let logobottom = document.querySelector(".custom-logo-desktop-bottom");
+  nav.classList.toggle("sticky",window.scrollY > 500)
+  if (window.scrollY > 500) {
+    logobottom.src = "jewel/pictures/logo-jewel-bottom-black.png";
+  } else if (window.scrollY < 200) {
+    logobottom.src = "jewel/pictures/logo-jewel-bottom.png";
   }
-  prevScrollpos = currentScrollPos;
-}
+})
+
