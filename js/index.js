f7c107e324b6f906fe8ee7919cd8d8fe2c3584ce
mejdikazdarlli <89795915+mejdikazdarlli@users.noreply.github.com>
2023-08-22 11:04:10 +0100

    update the design with image band

--- js/index.js
+++ js/index.js
@@ -10,3 +10,18 @@ window.addEventListener("scroll",function(){
   }
 })
 
+
+var slideIndex = 1;
+showDivs(slideIndex);
+window.plusDivs = (n) => { showDivs(slideIndex += n);}
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "flex";
+}
+
