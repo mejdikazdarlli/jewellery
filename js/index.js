2b891eb1205f48ed3933238d8f63b46d60ad7fcb
mejdikazdarlli <89795915+mejdikazdarlli@users.noreply.github.com>
2023-08-29 10:52:51 +0100

    add slider full Screen

--- js/index.js
+++ js/index.js
@@ -3,11 +3,11 @@ window.addEventListener("scroll",function(){
   let nav = document.querySelector(".custom-header-jewel");
   let logobottom = document.querySelector(".custom-logo-desktop-bottom");
   nav.classList.toggle("sticky",window.scrollY > 500)
-  if (window.scrollY > 500) {
-    logobottom.src = "jewel/pictures/logo-jewel-bottom-black.png";
-  } else if (window.scrollY < 200) {
-    logobottom.src = "jewel/pictures/logo-jewel-bottom.png";
-  }
+  // if (window.scrollY > 500) {
+  //   logobottom.src = "jewel/pictures/logo-jewel-bottom-black.png";
+  // } else if (window.scrollY < 200) {
+  //   logobottom.src = "jewel/pictures/logo-jewel-bottom.png";
+  // }
 })
 
 
@@ -16,7 +16,7 @@ showDivs(slideIndex);
 window.plusDivs = (n) => { showDivs(slideIndex += n);}
 function showDivs(n) {
   var i;
-  var x = document.getElementsByClassName("mySlides");
+  var x = document.getElementsByClassName("slide");
   if (n > x.length) {slideIndex = 1}
   if (n < 1) {slideIndex = x.length}
   for (i = 0; i < x.length; i++) {
